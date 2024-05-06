3a1dd9d6016a5d5d62a9bea178c86595eb267869
mariyam840 <159921630+mariyam840@users.noreply.github.com>
2024-05-06 12:36:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 OWNER_ID = int(getenv("OWNER_ID", "6449309718"))
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "ArmanChatz")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "Arman_Chatz")
 UPDATE_CHNL = getenv("UPDATE_CHNL", "Dil_Ka_Ehsaas")
 OWNER_USERNAME = getenv("OWNER_USERNAME", "Mr_Arman5")
 
